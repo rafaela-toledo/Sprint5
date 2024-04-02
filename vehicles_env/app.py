018daa3b7618d1e427c20cdd2d182b7f04c172a6
Rafaela Toledo <rafaelagotoledo@gmail.com>
2024-04-02 12:33:12 -0300

    Alt.4

--- vehicles_env/app.py
+++ vehicles_env/app.py
@@ -1,23 +1,19 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-df = pd.read_csv('/Users/rafaelatoledo/Desktop/MeuProjeto5/vehicles_env/vehicles.csv')
+df = pd.read_csv('/Users/rafaelatoledo/Desktop/MeuProjeto5/vehicles.csv')
 st.header('Análise Exploratória de Dados de Veículos')
 import pandas as pd
-     import plotly.express as px
-     import streamlit as st
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('/Users/rafaelatoledo/Desktop/MeuProjeto5/vehicles.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
      
-     car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
-     hist_button = st.button('Criar histograma') # criar um botão
-     
-     if hist_button: # se o botão for clicado
-         # escrever uma mensagem
-         st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+if hist_button: # se o botão for clicado
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
          
-         # criar um histograma
-         fig = px.histogram(car_data, x="odometer")
-     
-         # exibir um gráfico Plotly interativo
-         st.plotly_chart(fig, use_container_width=True)
-
-
+         
